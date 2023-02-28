56dbf75e33c95de09d690e9981b0eea8f02cffc7
Anas Osama <anasosama914@gmail.com>
2023-02-28 12:02:30 +0200

    Posts API
    Build Post database model,
    Serialize Post Model,
    Build 2 endpoints one for the List of Posts that has AllowAny permission,
    and second endpoint for retrieve, update, delete single Post from our database.

---
+++ backend/posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    thumb = models.ImageField(blank=True, null=True, upload_to='media/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.title
--- backend/root/urls.py
+++ backend/root/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
-    
+    path('api/v1/', include('api.urls'))
+    # path('', include('pages.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('accounts.urls')),
+
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
