437af27564e0bf772d54e6241e67b592deffb29d
Anas Osama <anasosama914@gmail.com>
2023-06-17 18:21:06 +0300

    MCI Model API

--- backend/root/urls.py
+++ backend/root/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    
+    path('prediction/api/v1/', include('prediction.urls')),
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
